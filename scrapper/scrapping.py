02aa55ea6b745a9703d7e0a462d1656997dac01d
Saul <camas17st@outlook.com>
2026-09-01 16:27:15 -0600

    Initial commit

---
+++ scrapper/scrapping.py
@@ -0,0 +1,8 @@
+import playwright
+import psycopg
+import bs4
+
+print('Scrapper set up!')
+print(playwright)
+print(psycopg.__version__)
+print(bs4.__version__)
---
+++ sql/init.sql
@@ -0,0 +1,47 @@
+/* Create tables */
+
+CREATE TABLE IF NOT EXISTS web_sources (
+   id_web_src SERIAL PRIMARY KEY,
+   url TEXT UNIQUE,
+   title TEXT
+);
+
+CREATE TABLE IF NOT EXISTS owner_contacts (
+   id_owner_contact SERIAL PRIMARY KEY,
+   profile_link TEXT UNIQUE,
+   name TEXT,
+   phone TEXT
+);
+
+CREATE TABLE IF NOT EXISTS raw_posts (
+   id_raw_post SERIAL PRIMARY KEY,
+   id_web_src INTEGER REFERENCES web_sources(id_web_src),
+   id_owner_contact INTEGER REFERENCES owner_contacts(id_owner_contact),
+   post_link TEXT UNIQUE,
+   raw_text TEXT UNIQUE
+);
+
+CREATE TABLE IF NOT EXISTS cars (
+   id_car SERIAL PRIMARY KEY,
+   id_raw_post INTEGER REFERENCES raw_posts(id_raw_post),
+   brand TEXT,
+   model TEXT NOT NULL,
+   trim TEXT,
+   year INTEGER NOT NULL,
+   transmission TEXT,
+   km INTEGER,
+   color TEXT,
+   condition TEXT,
+   owners INTEGER,
+   price INTEGER NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS cars_images (
+   id_img SERIAL PRIMARY KEY,
+   id_raw_post INTEGER REFERENCES raw_posts(id_raw_post),
+   img_1 BYTEA,
+   img_2 BYTEA,
+   img_3 BYTEA,
+   img_4 BYTEA,
+   img_5 BYTEA
+);
